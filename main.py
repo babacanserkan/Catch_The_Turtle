4f42641096c27f1adf006107f9b6da29d72a367c
Serkan Babacan <Fhourch4n@gmail.com>
2023-05-23 18:47:51 +0300

    make turtle & setup turtles complated

--- main.py
+++ main.py
@@ -15,7 +15,31 @@ def setup_score_turtle():
     score_turtle.goto(0, y)
     score_turtle.write("Score: 0", align="center", font=FONT)
 
+grid_size = 10
+
+def make_turtle(x, y):
+    t = turtle.Turtle()
+
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2, 2)
+    t.color("dark green")
+    t.goto(x * grid_size, y * grid_size)
+
+x_coordinates = [-20, -10, 0, 10, 20]
+y_coordinates = [20, 10, 0, -10]
+
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x, y)
 
 
+turtle.tracer(0)
+
+setup_turtles()
 setup_score_turtle()
+
+turtle.tracer(1)
+
 screen.mainloop()
